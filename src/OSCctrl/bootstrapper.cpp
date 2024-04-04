d7ae0d6963be4876c1912d7988345828a77f888c
will floyd <floyd.will@gmail.com>
2024-04-04 14:31:29 -0700

    implement Bootstrapper::addCtrl

--- src/OSCctrl/bootstrapper.cpp
+++ src/OSCctrl/bootstrapper.cpp
@@ -3,36 +3,81 @@
 
 Bootstrapper::Bootstrapper() {
   getCtrlJson();
-
-  /* addCtrl(); */
+  /* addCtrl(std::string("C:/VCV/test.vcv")); */
 }
 
-void Bootstrapper::addCtrl(const std::string& patchPath) {
-  /* std::string jsonPath = tempDir + "/patch.json"; */
-
-  /* std::fclose(file); */
-  /* DEBUG("removeRecursively(tempDir) %d", removeRecursively(tempDir)); */
-  /* DEBUG("remove(tempDir + '/modules') %d", remove(tempDir + "/modules")); */
-  /* DEBUG("remove(tempDir + '/patch.json') %d", remove(tempDir + "/patch.json")); */
-  /* DEBUG("remove(tempDir) %d", remove(tempDir)); */
-}
-
-void Bootstrapper::removeCtrl(const std::string& patchPath) {
-}
-
-std::string Bootstrapper::extractPatch(const std::string& patchPath) {
+void Bootstrapper::addCtrl(const std::string& patchArchivePath) {
   using namespace rack::system;
 
-  /* std::string tempDir = rack::asset::user(getStem(patchPath)); */
-  std::string tempDir = getTempDirectory() + getStem(patchPath);
+  std::string patchTempDir = extractPatch(patchArchivePath);
+  std::string jsonPath =  patchTempDir + "/patch.json";
+
+  FILE* file = std::fopen(jsonPath.c_str(), "r");
+  if (!file) {
+    FATAL("could not open OSCctrl donor patch file %s", jsonPath.c_str());
+    return;
+  }
+
+  json_error_t error;
+  json_t* rootJ = json_loadf(file, 0, &error);
+  if (!rootJ) {
+    FATAL("failed to parse OSCctrl donor patch json at %s %d:%d %s", error.source, error.line, error.column, error.text);
+    return;
+  }
+  DEFER({ json_decref(rootJ); });
+
+  json_t* modulesJ = json_object_get(rootJ, "modules");
+  DEFER({ json_decref(modulesJ); });
+
+  std::string modelName;
+  /* std::vector<int64_t> moduleIds; */
+
+  size_t index;
+  json_t *value;
+  json_array_foreach(modulesJ, index, value) {
+    /* moduleIds.push_back(json_integer_value(json_object_get(value, "id"))); */
+
+    modelName =
+      std::string(json_string_value(json_object_get(value, "model")));
+    if (modelName == "OSCctrl") {
+      std::fclose(file);
+      removeRecursively(patchTempDir);
+      DEBUG("OSCctrl already exists, aborting bootstrap");
+      return;
+    }
+  }
+
+  // TODO: address unlikely id collision, recursify?
+  /* int64_t ctrlId = json_integer_value(json_object_get(ctrlJson, "id")); */
+  /* for (int64_t& moduleId : moduleIds) { */
+  /*   if (moduleId == ctrlId) ctrlId++; */
+  /* } */
+  json_array_append(modulesJ, ctrlJson);
+  json_dump_file(rootJ, jsonPath.c_str(), JSON_INDENT(2));
+
+  archiveDirectory(patchArchivePath, patchTempDir, 1);
+
+  std::fclose(file);
+  removeRecursively(patchTempDir);
+}
+
+void Bootstrapper::removeCtrl(const std::string& patchArchivePath) {
+}
+
+std::string Bootstrapper::extractPatch(const std::string& patchArchivePath) {
+  using namespace rack::system;
+
+  /* std::string tempDir = rack::asset::user(getStem(patchArchivePath)); */
+  std::string tempDir = getTempDirectory() + getStem(patchArchivePath);
+  removeRecursively(tempDir);
   if (!createDirectories(tempDir)) {
     DEBUG("bootstrapper extractPatch: unable to create tempdir: %s", tempDir.c_str());
     return std::string("");
   }
 
-  DEBUG("extracting %s to %s", patchPath.c_str(), tempDir.c_str());
+  DEBUG("extracting %s to %s", patchArchivePath.c_str(), tempDir.c_str());
 
-  unarchiveToDirectory(patchPath, tempDir);
+  unarchiveToDirectory(patchArchivePath, tempDir);
 
   return tempDir;
 }
@@ -65,9 +110,11 @@ void Bootstrapper::getCtrlJson() {
 
   size_t index;
   json_t *value;
+  std::string modelName;
 
   json_array_foreach(modulesJ, index, value) {
-    std::string modelName(json_string_value(json_object_get(value, "model")));
+    modelName =
+      std::string(json_string_value(json_object_get(value, "model")));
     if (modelName == "OSCctrl") {
       ctrlJson = json_deep_copy(value);
       break;
