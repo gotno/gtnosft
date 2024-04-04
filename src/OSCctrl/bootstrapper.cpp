ce7bbb05e67c3a2f0ea0bb192acdedaa9f3828d5
will floyd <floyd.will@gmail.com>
2024-04-04 14:37:40 -0700

    implement Bootstrapper::removeCtrl

--- src/OSCctrl/bootstrapper.cpp
+++ src/OSCctrl/bootstrapper.cpp
@@ -3,7 +3,6 @@
 
 Bootstrapper::Bootstrapper() {
   getCtrlJson();
-  /* addCtrl(std::string("C:/VCV/test.vcv")); */
 }
 
 void Bootstrapper::addCtrl(const std::string& patchArchivePath) {
@@ -62,6 +61,50 @@ void Bootstrapper::addCtrl(const std::string& patchArchivePath) {
 }
 
 void Bootstrapper::removeCtrl(const std::string& patchArchivePath) {
+  using namespace rack::system;
+
+  std::string patchTempDir = extractPatch(patchArchivePath);
+  std::string jsonPath =  patchTempDir + "/patch.json";
+
+  FILE* file = std::fopen(jsonPath.c_str(), "r");
+  if (!file) {
+    FATAL("could not open OSCctrl donor patch file %s", jsonPath.c_str());
+    return;
+  }
+
+  json_error_t error;
+  json_t* rootJ = json_loadf(file, 0, &error);
+  if (!rootJ) {
+    FATAL("failed to parse OSCctrl donor patch json at %s %d:%d %s", error.source, error.line, error.column, error.text);
+    return;
+  }
+  DEFER({ json_decref(rootJ); });
+
+  json_t* modulesJ = json_object_get(rootJ, "modules");
+  DEFER({ json_decref(modulesJ); });
+
+  std::string modelName;
+
+  int64_t ctrlIndex{-1};
+  size_t index;
+  json_t *value;
+  json_array_foreach(modulesJ, index, value) {
+    modelName =
+      std::string(json_string_value(json_object_get(value, "model")));
+    if (modelName == "OSCctrl") {
+      ctrlIndex = index;
+      break;
+    }
+  }
+
+  if (ctrlIndex != -1) {
+    json_array_remove(modulesJ, ctrlIndex);
+    json_dump_file(rootJ, jsonPath.c_str(), JSON_INDENT(2));
+    archiveDirectory(patchArchivePath, patchTempDir, 1);
+  }
+
+  std::fclose(file);
+  removeRecursively(patchTempDir);
 }
 
 std::string Bootstrapper::extractPatch(const std::string& patchArchivePath) {
